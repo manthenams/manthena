d499730ae6368bf6fdbf2203edb56a6790acab94
manthenams <52356125+manthenams@users.noreply.github.com>
2019-06-29 23:04:02 +0530

    Update HelloServiceTest.java

--- numbertoword/src/test/java/com/numberword/core/services/HelloServiceTest.java
+++ numbertoword/src/test/java/com/numberword/core/services/HelloServiceTest.java
@@ -1,29 +1,32 @@
 package com.numberword.core.services;
 
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Assertions;
 
 public class HelloServiceTest {   
-static ConvertServiceImpl convert=new ConvertServiceImpl();
 
  @Test
     void testGet() {
-int number = 0;
-		Scanner scanner = new Scanner(System.in);
+int number = 10;
+	//	Scanner scanner = new Scanner(System.in);
 		System.out.println("Please type a number(max upto 9 digits)");
 		try {
 			// read the number
-			number = scanner.nextInt();
+			//number = scanner.nextInt();
 			if (number == 0) {
 				System.out.print("Number in words: Zero");
-			} else {				assertEquals("one",convert.numberToWord(number));
+			} else {		
+      assertEquals("ten",ConvertServiceImpl.getInstance().numberToWord(number));
+
 			}
 		} catch (Exception e) {
 			System.out.println("Please enter a valid number");
 		}
 		// close the reader
-		scanner.close();
+	//	scanner.close();
         
     }
 
